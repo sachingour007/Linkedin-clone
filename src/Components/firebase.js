4cbd69544305f731ea1672acefa57a11fb862d78
sachingour <gour.sachin.sg@gmail.com>
2023-06-06 12:10:22 +0530

    updates

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC3AQA6ULnJBU1rGMHFJlFRcTKIofz-SE8",
-    authDomain: "linkein-clone-007.firebaseapp.com",
-    projectId: "linkein-clone-007",
-    storageBucket: "linkein-clone-007.appspot.com",
-    messagingSenderId: "766737353310",
-    appId: "1:766737353310:web:12ac48c19d5128b09642cf"
-  };
+  apiKey: "AIzaSyC3AQA6ULnJBU1rGMHFJlFRcTKIofz-SE8",
+  authDomain: "linkein-clone-007.firebaseapp.com",
+  projectId: "linkein-clone-007",
+  storageBucket: "linkein-clone-007.appspot.com",
+  messagingSenderId: "766737353310",
+  appId: "1:766737353310:web:488afed1cdaef00c9642cf"
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
